e0488740dd7b4d74366b32f7618212cb9e51ee72
Bala Shanmugam <compsciuniv@gmail.com>
2024-03-06 09:35:05 +0530

    Return the response

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from chatbot import CB
+#from chatbot import CB
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/chatbot")
 def home():
+    print("Chatbot route:")
     return render_template("index.html")
 
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
-    return str(CB.get_response(userText))
+    print("get route:", userText)
+    return userText + " Received"
 app.run(debug = True)
